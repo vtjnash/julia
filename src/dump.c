ba24d824e8480ad30d4cc352a7ae54064c19e555
Jameson Nash <vtjnash@gmail.com>
2020-02-10 22:41:45 -0500

    bugfix
    fixes #29859

--- src/dump.c
+++ src/dump.c
@@ -2462,7 +2462,6 @@ static void jl_reinit_item(jl_value_t *v, int how, arraylist_t *tracee_list)
             case 3: { // rehash MethodTable
                 jl_methtable_t *mt = (jl_methtable_t*)v;
                 jl_typemap_rehash(mt->defs, 0);
-                // TODO: consider reverting this when we can split on Type{...} better
                 jl_typemap_rehash(mt->cache, mt->offs);
                 if (tracee_list)
                     arraylist_push(tracee_list, mt);
